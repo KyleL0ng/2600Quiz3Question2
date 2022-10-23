d336426356127a234389364d370352e34a35acc7
Kyle Long <kjlong@cpp.edu>
2022-10-23 00:28:56 -0700

    Added code for all required functions, no testing

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -1,9 +1,46 @@
-int main(double taxAmount, double tipAmount) {
- //set variables to the values passed into main
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
- //pick random meal cost
+int main(double taxPercent, double tipPercent)
+{
+    // set up needed variables
+    double mealCost = 0;
+    double tipCost = 0;
+    double taxCost = 0;
+    double totalCost = 0;
 
- //compute tax and tip for the meal price
+    // pick random meal cost
+    time_t t;
+    srand((unsigned)time(&t));
+    int menuChoice = rand() % 4; // gets random choice from the computer
 
- //print the meal cost, tax amount, tip amount, and total
+    switch (menuChoice)
+    {
+    case 0:
+        mealCost = 9.95;
+        break;
+    case 1:
+        mealCost = 4.55;
+        break;
+    case 2:
+        mealCost = 13.25;
+        break;
+    case 3:
+        mealCost = 22.35;
+        break;
+    } // end switch
+
+    // compute tax and tip for the meal price
+    tipCost = mealCost * tipPercent;
+    taxCost = mealCost * taxPercent;
+
+    // compute total price for the bill
+    totalCost = mealCost + tipCost + taxCost;
+
+    // print the meal cost, tax amount, tip amount, and total
+    printf("The meal costs %d\n", mealCost);
+    printf("The tax costs %d\n", taxCost);
+    printf("The tip costs %d\n", tipCost);
+    printf("The bill's total comes out to: %d", totalCost);
 }
