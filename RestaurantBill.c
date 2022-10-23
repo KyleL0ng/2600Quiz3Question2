be7bd9f25774c1cb1beffb711ed87bcc3c841dcd
Kyle Long <kjlong@cpp.edu>
2022-10-23 00:02:44 -0700

    Initial commit with layout

---
+++ RestaurantBill.c
@@ -0,0 +1,9 @@
+int main(double taxAmount, double tipAmount) {
+ //set variables to the values passed into main
+
+ //pick random meal cost
+
+ //compute tax and tip for the meal price
+
+ //print the meal cost, tax amount, tip amount, and total
+}
